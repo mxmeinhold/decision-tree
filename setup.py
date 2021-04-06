daca9b85ee911426fc24300635fba743eaaa8304
Max Meinhold <mxmeinhold@gmail.com>
2021-04-06 00:25:32 -0400

    Change package name
    `decision-tree` was unavailable on pypi, so this is now
    `decision-tree-typed`

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="decision-tree",
+    name="decision-tree-typed",
     version="0.1.0",
     author="Max Meinhold",
     author_email="mxmeinhold@gmail.com",
